ff27d44e9886be919f79b512c93bdda7b2f0d8cd
ByggarN <emrik.bergman@icloud.com>
2022-11-08 10:12:22 +0100

    readPos done

--- servoPosReader.ino
+++ servoPosReader.ino
@@ -3,11 +3,11 @@
 
 // https://github.com/users/Saud0227/projects/1
 
-int readPins [8] = {0, 1, 2, 3, 4, 5, 6, 7};
+int readPins [8] = {A0, A1, A2, A3, A4, A5, 3, 5};
 
 
 
-
+/*
 void setup(){
     Serial.begin(12500);
 }
@@ -21,4 +21,36 @@ int getPos() {
 void loop(){
     delay(1);
     getPos();
+}
+
+
+*/
+
+int analog_IN = A0;  // This is our input pin
+int digital_OUT = 13;
+
+void setup() { 
+    pinMode(digital_OUT, 1);
+    pinMode(analog_IN, 0);
+    Serial.begin(9600);
+  
+}
+
+void loop() {
+    int outVal = readVal(0);
+    Serial.print(outVal);
+    if (outVal) {
+      digitalWrite(digital_OUT, HIGH);
+    }else{
+      digitalWrite(digital_OUT, LOW);
+    }
+}
+
+bool readVal(int iToRead){
+    int Value = analogRead(readPins[iToRead]);
+    Serial.print("Analog read: ");
+    Serial.println(Value);  
+    delay(200);
+    return (Value < 500);
+    
 }
